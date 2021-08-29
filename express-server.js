de7c5e4aecc794cb1969d4d31e6c4f2d6cdd2fc3
Gareth <gar.carey@gmail.com>
2021-08-29 10:03:42 -0400

    add post for urls when entering new url

--- express-server.js
+++ express-server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -42,16 +41,6 @@ app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
-
-function generateRandomString() {
-  let randomString = ""
-  const possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-  for(let i = 0; i < 6; i++){
-    randomString = possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
-  }
-  return randomString;
-}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
