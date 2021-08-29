c98f31389d4e5345236b5d49edf89e5564019921
Gareth <gar.carey@gmail.com>
2021-08-29 10:02:14 -0400

    get for urls/new page and install body parser as dependency

--- express-server.js
+++ express-server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser');
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,10 +29,29 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  let randomString = ""
+  const possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  for(let i = 0; i < 6; i++){
+    randomString = possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+  return randomString;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
