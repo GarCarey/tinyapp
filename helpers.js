aa7f973139d991e4474f17360e88884d249e6cd3
Gareth <gar.carey@gmail.com>
2021-09-06 13:00:32 -0400

    move findUserByEmail from server file to here

---
+++ helpers.js
@@ -0,0 +1,11 @@
+function findUserByEmail(email, database) {
+  for (const id in database) {
+    const user = database[id];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+module.exports = { findUserByEmail };
