177530f2b6164957cf3f574d9ffad3793276facb
Gareth <gar.carey@gmail.com>
2021-09-06 14:37:28 -0400

    add helper functions from express-server.js

--- helpers.js
+++ helpers.js
@@ -8,4 +8,27 @@ function findUserByEmail(email, database) {
   return undefined;
 };
 
-module.exports = { findUserByEmail };
+function generateRandomString() {
+  let randomString = ""
+  const possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  for (let i = 0; i < 6; i++) {
+    randomString += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+  return randomString;
+};
+
+function urlsForUser(id, database) {
+  let newDB = {};
+
+  for (const shortURL in database) {
+    if (database[shortURL].userID === id) {
+      newDB[shortURL] = {
+        longURL: database[shortURL].longURL,
+      };
+    }
+  }
+  return newDB;
+};
+
+module.exports = { findUserByEmail, generateRandomString, urlsForUser };
